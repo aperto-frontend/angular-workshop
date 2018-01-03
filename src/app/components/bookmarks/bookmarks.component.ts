5662197fc2e76763f3014023b386caa510c24bd6
Podolski, Henri <Henri.Podolski@aperto.com>
2018-01-03 10:33:46 +0100

    Add mock data and change bookmarks template to output this data

--- src/app/components/bookmarks/bookmarks.component.ts
+++ src/app/components/bookmarks/bookmarks.component.ts
@@ -7,6 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookmarksComponent implements OnInit {
 
+  bookmarks = [
+    {
+      id: 1,
+      title: 'Tour of Heroes',
+      url: 'https://angular.io/tutorial'
+    },
+    {
+      id: 2,
+      title: 'CLI Documentation',
+      url: 'https://github.com/angular/angular-cli/wiki'
+    },
+    {
+      id: 3,
+      url: 'https://blog.angular.io/'
+    },
+    {
+      id: 4,
+      title: 'Angular Home'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
